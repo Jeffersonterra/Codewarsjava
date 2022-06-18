61e634bbe83d657fdf1995b380e3fc1288f877aa
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-18 17:10:04 -0300

    Create Upper.java

---
+++ Upper.java
@@ -0,0 +1,10 @@
+
+class Upper {
+    public static String MakeUpperCase(String str) {
+        // from code wars
+        String s1 = str;
+        String s1upper = s1.toUpperCase();
+        return s1upper;
+    }
+
+}
